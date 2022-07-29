d3732608f6f06d03a9a3bbe9ac317a6cc4e4f946
Taylor Bromley <taylorbromley@sky.com>
2022-07-29 14:23:12 +0100

    Input error handling. Double select and where conditions implemented

--- SakilaBackend/Controllers/ActorController.cs
+++ SakilaBackend/Controllers/ActorController.cs
@@ -15,27 +15,78 @@ namespace SakilaBackend.Controllers
         [HttpGet("/getallactors")]
         public List<Actor> GetActors()
         {
-
             List<Actor> actors;
             using (var context = new sakilaContext())
             {
                 actors = context.Actors.ToList();
             }
-
+            return actors;
+        }
+        
+        [HttpGet("/getnamesofallactors")]
+        public IQueryable GetActorsByName()
+        {
+            IQueryable actors;
+            using (var context = new sakilaContext())
+            {
+                actors = context.Actors.
+                    Select(a => new
+                    {
+                        a.FirstName,
+                        a.LastName
+                    }).
+                    ToList().
+                    AsQueryable();
+            }
             return actors;
         }
 
         [HttpGet("/getactor/{actorId:int}")]    //error handling for if the id does not exist
-        public string GetActor(int actorId)
+        public IQueryable GetActor(int actorId)
         {
-            using (var context = new sakilaContext())
+            try
             {
-                string actor = context.Actors.
-                    Where(a => a.ActorId.Equals((short)actorId)).
-                    Select(a => a.FirstName).
-                    First();
-
+                IQueryable actor;
+                using (var context = new sakilaContext())
+                {
+                    actor = context.Actors.
+                        Where(a => a.ActorId.Equals((short)actorId)).
+                        Select(a => new
+                        {
+                            a.FirstName,
+                            a.LastName
+                        }).
+                        ToList().
+                        AsQueryable();
+                }
                 return actor;
+            }
+            catch
+            {
+                Console.WriteLine("actorId does not exist");
+                return null;
+            }
+        }
+
+        [HttpGet("/getactorid/{firstname=string}/{lastname=string}")]
+        public short GetActorId(string firstname, string lastname)
+        {
+            try
+            {
+                short actor;
+                using (var context = new sakilaContext())
+                {
+                    actor = context.Actors.
+                        Where(a => a.FirstName.Equals(firstname) && (a.LastName.Equals(lastname))).
+                        Select(a => a.ActorId).
+                        First();
+                }
+                return actor;
+            }
+            catch
+            {
+                Console.WriteLine("Actor with that name does not exist");
+                return 0;
             }
         }
 
@@ -55,26 +106,40 @@ namespace SakilaBackend.Controllers
         [HttpPatch("/updateexistingactorname/{originalfirstname=string}/{newfirstname=string}")]    //error handling for if the original name does not exist
         public void PatchActorName(string originalfirstname, string newfirstname)
         {
-            using (var context = new sakilaContext())
+            try
             {
-                Actor actor = context.Actors.
-                    Where(a => a.FirstName.Equals(originalfirstname)).
-                    First();
-                actor.FirstName = newfirstname;
-                context.SaveChanges();
+                using (var context = new sakilaContext())
+                {
+                    Actor actor = context.Actors.
+                        Where(a => a.FirstName.Equals(originalfirstname)).
+                        First();
+                    actor.FirstName = newfirstname;
+                    context.SaveChanges();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Actor with that name does not exist");
             }
         }
 
         [HttpDelete("/deleteactor/{actorId:int}")]    //error handling for if the id does not exist
         public void DeleteActorName(int actorId)
         {
-            using (var context = new sakilaContext())
+            try
             {
-                Actor actor = context.Actors.
-                    Where(a => a.ActorId.Equals((short)actorId)).
-                    First();
-                context.Actors.Remove(actor);
-                context.SaveChanges();
+                using (var context = new sakilaContext())
+                {
+                    Actor actor = context.Actors.
+                        Where(a => a.ActorId.Equals((short)actorId)).
+                        First();
+                    context.Actors.Remove(actor);
+                    context.SaveChanges();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Actor with that ID does not exist");
             }
         }       
     }
